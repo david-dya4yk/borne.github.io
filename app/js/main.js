3a3ffcb15ac024135d430e19c4fad5e041118b5e
david-dya4yk <daviddya4yk@mail.ru>
2022-11-30 00:50:28 +0200

    added section teams

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,5 @@
+// const { default: Swiper } = require("swiper");
+
 $(function(){
      new Swiper(".swiper",{
         direction: "vertical",
@@ -10,6 +12,16 @@ $(function(){
         //     delay: 3000,
         //     },
      });
+
+     new Swiper(".teams",{
+      slidesPerView: 3,
+      slidesPerGroup: 1,
+      spaceBetween: 50,
+      containerModifierClass: "teams-",
+      slideClass: "teams-slide",
+      wrapperClass: "teams-wrapper",
+      
+   });
      Fancybox.bind("[data-fancybox]", {
       
     });
