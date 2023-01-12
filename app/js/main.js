16b7e1945ab56135d394f5978206f831512b6821
david-dya4yk <daviddya4yk@mail.ru>
2023-01-12 23:26:16 +0200

    added single page

--- app/js/main.js
+++ app/js/main.js
@@ -23,6 +23,34 @@ $(
       wrapperClass: "teams-wrapper",
     });
 
+    (function quantityProducts() {
+      var $quantityArrowMinus = $(".single__minus");
+      var $quantityArrowPlus = $(".single__plus");
+      var $quantityNum = $(".single__num");
+  
+      $quantityArrowMinus.click(quantityMinus);
+      $quantityArrowPlus.click(quantityPlus);
+  
+      function quantityMinus() {
+        if ($quantityNum.val() > 1) {
+          $quantityNum.val(+$quantityNum.val() - 1);
+        }
+      }
+  
+      function quantityPlus() {
+        $quantityNum.val(+$quantityNum.val() + 1);
+      }
+    })();
+
+    $(".tabs__link").on("click", function (e) {
+      e.preventDefault();
+      $(".tabs__link").removeClass("tabs__link--active");
+      $(this).addClass("tabs__link--active");
+  
+      $(".tabs__item").removeClass("tabs__item--active");
+      $($(this).attr("href")).addClass("tabs__item--active");
+    });
+
     // $(".post__item").on("click", function () {˛
     //   $("post__item").toggleClass("post__item");
     //   $(this).addClass("post__item--active");
